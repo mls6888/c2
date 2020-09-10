340bf30cf17cf3fba9810b30cf3aec331eb5be81
mls6888 <mls6888@psu.edu>
2020-09-10 21:02:31 +0000

    k

--- main.c
+++ main.c
@@ -4,52 +4,46 @@
 // Collaborator: Fletcher Henneman fdh5031@psu.edu 
 // Collaborator: Brian Chetle bjc5969@psu.edu
 //  Breakout11
-
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 
 
 int main(void) {
-
- char *letter = "F";
- char *x = readline("Enter your CMPSC 131 grade: ");
- double grade = atof(x);
- if (grade >= 93.0 ) {
- letter = "A";
+double grade = atof(readline("Enter your CMPSC 131 grade: "));
+ 
+if (grade >= 93.0 ) {
+ printf("Your letter grade for CMPSC 131 is A.\n");
  }
-  else if (grade >= 90.0 ) {
- letter = "A-";
+else if (grade >= 90.0 ) {
+  printf("Your letter grade for CMPSC 131 is A-.\n");
  }  
-  else if (grade >= 87.0 ) {
- letter = "B+";
+else if (grade >= 87.0 ) {
+  printf("Your letter grade for CMPSC 131 is B+.\n.");
  }  
-  else if (grade >= 83.0 ) {
- letter = "B";
+else if (grade >= 83.0 ) {
+  printf("Your letter grade for CMPSC 131 is B.\n.");
  }  
-  else if (grade >= 80.0 ) {
- letter = "B-";
+else if (grade >= 80.0 ) {
+  printf("Your letter grade for CMPSC 131 is B-.\n.");
  }  
-  else if (grade >= 77.0) {
- letter = "C+";
+else if (grade >= 77.0) {
+  printf("Your letter grade for CMPSC 131 is C+.\n.");
  }  
-  else if (grade >= 70.0) {
- letter = "C";
+else if (grade >= 70.0) {
+ printf("Your letter grade for CMPSC 131 is C.\n.");
  }  
-  else if (grade >= 60.0) {
- letter = "D";
+else if (grade >= 60.0) {
+ printf("Your letter grade for CMPSC 131 is D.\n.");
  }   
-  else { 
- letter = "F";
- }  
+else { 
+ printf("Your letter grade for CMPSC 131 is F.\n.");
+ }   
   
  
  
  
- printf("Your letter grade for CMPSC 131 is %s",letter);
+ 
  return 0;
 
 
