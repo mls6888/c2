6dc55458c5b6ed04b81009bdf58625b95c9dd065
mls6888 <mls6888@psu.edu>
2020-09-10 21:09:01 +0000

    p

--- main.c
+++ main.c
@@ -19,25 +19,25 @@ else if (grade >= 90.0 && grade <=93.0) {
   printf("Your letter grade for CMPSC 131 is A-.\n");
  }  
 else if (grade >= 87.0 && grade <=90.0) {
-  printf("Your letter grade for CMPSC 131 is B+.\n.");
+  printf("Your letter grade for CMPSC 131 is B+.\n");
  }  
 else if (grade >= 83.0 && grade <=87.0) {
-  printf("Your letter grade for CMPSC 131 is B.\n.");
+  printf("Your letter grade for CMPSC 131 is B.\n");
  }  
 else if (grade >= 80.0 && grade <=83.0) {
-  printf("Your letter grade for CMPSC 131 is B-.\n.");
+  printf("Your letter grade for CMPSC 131 is B-.\n");
  }  
 else if (grade >= 77.0 && grade <=80.0) {
-  printf("Your letter grade for CMPSC 131 is C+.\n.");
+  printf("Your letter grade for CMPSC 131 is C+.\n");
  }  
 else if (grade >= 70.0 && grade <=77.0) {
- printf("Your letter grade for CMPSC 131 is C.\n.");
+ printf("Your letter grade for CMPSC 131 is C.\n");
  }  
 else if (grade >= 60.0 && grade <= 70.0) {
- printf("Your letter grade for CMPSC 131 is D.\n.");
+ printf("Your letter grade for CMPSC 131 is D.\n");
  }   
 else { 
- printf("Your letter grade for CMPSC 131 is F.\n.");
+ printf("Your letter grade for CMPSC 131 is F.\n");
  }   
   
  
